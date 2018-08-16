889ac12c7b7e0660a7210bbf7d1b6b3afa4d5c33
Hei Jing Tsang <tsangh@purdue.edu>
2018-08-16 17:40:07 +0800

    Changes to be committed: 	new file:   .idea/vcs.xml 	modified:   .idea/workspace.xml         new file:   __pycache__/app.cpython-36.pyc 	modified:   app.py 	modified:   requirements.txt 	modified:   templates/main.html 	new file:   venv/lib/python3.6/site-packages/__pycache__/itsdangerous.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/markupsafe/__pycache__/__init__.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/markupsafe/__pycache__/_compat.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/simplejson/__pycache__/__init__.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/simplejson/__pycache__/compat.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/simplejson/__pycache__/decoder.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/simplejson/__pycache__/encoder.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/simplejson/__pycache__/errors.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/simplejson/__pycache__/raw_json.cpython-36.pyc 	new file:   venv/lib/python3.6/site-packages/simplejson/__pycache__/scanner.cpython-36.pyc

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
-from flask import Flask, flash, render_template, request
+from flask import Flask, flash, render_template, request, redirect, url_for
 import json
 import requests
 
 app = Flask(__name__)
 
+translation = ""
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
+        if not request.form['text']:
+            flash('Please input text for translation.', 'Error')
+        else:
+            text = request.form['text']
+            choice = request.form['choice']
+            uri = "https://api.funtranslations.com/translate/" + choice + ".json"
+            payload = {'text': text}
 
-        text = request.form['text']
-        choice = request.form['choice']
-        uri = "https://api.funtranslations.com/translate/" + choice + ".json"
-        payload = {'text': text}
+            try:
+                uResponse = requests.get(uri, params=payload)
+            except requests.ConnectionError:
+                flash('Connection Error', 'Error')
+                return redirect(url_for('home'))
 
-        try:
-            uResponse = requests.get(uri, params=payload)
-        except requests.ConnectionError:
-            flash("Connection Error", 'error')
-            return "Connection Error"
+            jresponse = uResponse.text
+            data = json.loads(jresponse)
 
-        Jresponse = uResponse.text
-        data = json.loads(Jresponse)
+            translation = data['contents']['translated']
+            flash(choice + '\'s translation is: \n' + translation, 'Success')
 
-        translation = data['contents']['translated']
-
-    return render_template("main.html", translation=translation)
+    return render_template("main.html")
 
 
 if __name__ == '__main__':
